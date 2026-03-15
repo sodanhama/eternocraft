5361e3d0f7171aad53f366ddb05e54ead445193d
sodanhama <brainosoham@gmail.com>
2026-03-15 10:56:34 +0530

    added place and destroy block functions

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,6 +7,7 @@ const JUMP_VELOCITY = 12
 var sensitivity = 0.002
 
 @onready var camera_3d = $Camera3D
+@onready var ray_cast_3d= $Camera3D/RayCast3D
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -36,5 +37,17 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	if Input.is_action_just_pressed("left_click"):
+		if ray_cast_3d.is_colliding():
+			if ray_cast_3d.get_collider().has_method("destroy_block"):
+				ray_cast_3d.get_collider().destroy_block(ray_cast_3d.get_collision_point() - ray_cast_3d.get_collision_normal())
+	
+	if Input.is_action_just_pressed("right_click"):
+		if ray_cast_3d.is_colliding():
+			if ray_cast_3d.get_collider().has_method("place_block"):
+				ray_cast_3d.get_collider().place_block(ray_cast_3d.get_collision_point() + ray_cast_3d.get_collision_normal(),6)
+		
+	
 
 	move_and_slide()
