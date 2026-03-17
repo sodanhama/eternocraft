6da28a1bf744c7d19e4a766326aa203bd738d65a
sodanhama <brainosoham@gmail.com>
2026-03-17 09:32:15 +0530

    added block delete effects, hotbar and selection

--- scripts/grid_map.gd
+++ scripts/grid_map.gd
@@ -1,9 +1,14 @@
 extends GridMap
-
+const BLOCK_BREAK_PARTICLES = preload("res://scenes/block_break_particles.tscn")
+var instance
 
 func destroy_block(world_coordinate):
 	var map_coordinate = local_to_map(world_coordinate)
 	set_cell_item(map_coordinate,-1)
+	instance = BLOCK_BREAK_PARTICLES.instantiate()
+	instance.position = map_to_local(map_coordinate)
+	add_child(instance)
+	instance.emitting = true
 
 func place_block(world_coordinate, block_index):
 	var map_coordinate = local_to_map(world_coordinate)
--- scripts/player.gd
+++ scripts/player.gd
@@ -5,12 +5,16 @@ const SPEED = 8
 const JUMP_VELOCITY = 12
 
 var sensitivity = 0.002
+var selected = 2
 
 @onready var camera_3d = $Camera3D
 @onready var ray_cast_3d= $Camera3D/RayCast3D
+@onready var hotbar: ItemList = $Hotbar
+
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	hotbar.select(2)
 	
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
@@ -46,8 +50,19 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("right_click"):
 		if ray_cast_3d.is_colliding():
 			if ray_cast_3d.get_collider().has_method("place_block"):
-				ray_cast_3d.get_collider().place_block(ray_cast_3d.get_collision_point() + ray_cast_3d.get_collision_normal(),6)
+				ray_cast_3d.get_collider().place_block(ray_cast_3d.get_collision_point() + ray_cast_3d.get_collision_normal(),selected)
 		
 	
-
+	if Input.is_action_just_pressed("one"):
+		selected=2
+		hotbar.select(0)
+	if Input.is_action_just_pressed("two"):
+		selected=3
+		hotbar.select(1)
+	if Input.is_action_just_pressed("three"):
+		selected=8
+		hotbar.select(2)
+	if Input.is_action_just_pressed("four"):
+		selected=7
+		hotbar.select(3)	
 	move_and_slide()
